5a3adb49ab750215bcc19b2755550232e88aeb67
TamBurns <tamarZburns@gmail.com>
2022-01-26 11:23:55 +0000

    sql connection

--- proj/webapp/app.py
+++ proj/webapp/app.py
@@ -1,11 +1,29 @@
 
 from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_HOST'] = 'panda_db'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL()
+mysql.init_app(app)
+
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    cursor = mysql.connection.cursor()
+
+    cursor.execute(f"use panda_db;")
+    cursor.execute(f"SELECT * FROM 'cocktails';")
+    count = cursor.fetchall()
+    cursor.close()
+
+    return render_template("home.html", count = count)
 
 @app.route("/c_search")
 def c_search():
@@ -18,6 +36,10 @@ def s_search():
 @app.route("/newcard")
 def newcard():
     return render_template("newcard.html")
+
+@app.route("/help")
+def help():
+    return render_template("help.html")
 
 
 if __name__ == "__main__":
