56924cc64c2d88d1a7a8bbe8228ae77b42162227
TamBurns <tamarZburns@gmail.com>
2022-01-26 10:23:34 +0000

    page templates and routes

--- proj/webapp/app.py
+++ proj/webapp/app.py
@@ -7,9 +7,18 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/c_search")
+def c_search():
+    return render_template("c_search.html")
+
+@app.route("/s_search")
+def s_search():
+    return render_template("s_search.html")
+
 @app.route("/newcard")
 def newcard():
     return render_template("newcard.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
